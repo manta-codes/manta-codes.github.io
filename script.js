7026a6dff67103764757370effa339778e237764
Sam N <samathasaurus@gmail.com>
2024-11-19 22:07:35 -0500

    update mantis facts

--- script.js
+++ script.js
@@ -9,7 +9,14 @@
 
 //--- populate facts and prevent repeating facts ---//
 function displayfact() {
-    const facts = ["fact 1","fact 2","fact 3"]
+
+    const facts = 
+    [
+        "All mantis shrimp species can be divided into ‘spearers’ or ‘smashers’, based on the types of claws they have and tactics they use to kill the prey.",
+        "Mantis shrimp smashers can punch at same velocity as a gunshot from a .22 caliber rifle.",
+        "Their punch is so fast it results in ‘cavitation’ bubbles. This is a super heated bubble and small flash of light, which for split seconds also generates temperates of 4,400c (which is nearly as hot as the sun) in the surrounding water.",
+        "The mantis shrimp has 12-16 different colour photoreceptors for colour analysis in their retinas. Three times more than a human."
+    ]
     const random = Math.floor(Math.random() * facts.length)
 
     let randomFact = (random, facts[random]);
@@ -17,4 +24,4 @@ function displayfact() {
     document.querySelector('#mantisfacts').textContent = randomFact
 }
 
-setInterval(displayfact, 2000)
+setInterval(displayfact, 5000)
