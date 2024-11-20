cfd8d83100541e1f7b7dc6f3718aed5fc9b3d195
Sam N <samathasaurus@gmail.com>
2024-11-20 11:27:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-//--- display current date and time ---//
-
-// function updateDateTime() {
-   // const date = new Date();
-   // const currentDateTime = date.toLocaleString();
-   // document.querySelector('#currentDateTime').textContent = currentDateTime;
-// }
-// setInterval(updateDateTime, 1000)
-
-//--- populate facts and replace randomizer with for each to cycle through array ---//
-function displayfact() {
-
-    const facts = 
+const mantisFacts = 
     [
         "All mantis shrimp species can be divided into ‘spearers’ or ‘smashers’, based on the types of claws they have and tactics they use to kill the prey.",
         "Mantis shrimp smashers can punch at same velocity as a gunshot from a .22 caliber rifle.",
         "Their punch is so fast it results in ‘cavitation’ bubbles. This is a super heated bubble and small flash of light, which for split seconds also generates temperates of 4,400c (which is nearly as hot as the sun) in the surrounding water.",
         "The mantis shrimp has 12-16 different colour photoreceptors for colour analysis in their retinas. Three times more than a human."
     ]
-    const random = Math.floor(Math.random() * facts.length)
 
-    let randomFact = (random, facts[random]);
+let i = 0;
 
-    document.querySelector('#mantisfacts').textContent = randomFact
-}
+// need to identify how to prevent header from moving in footer and remove delay display fact when page loads
 
-setInterval(displayfact, 5000)
-
-
-
-//myArray.forEach(element => {
-  //  console.log(element);
+setInterval(() => {
+    const element = mantisFacts[i];
+    document.querySelector('#mantisfacts').textContent = element;
+    i = (i + 1) % mantisFacts.length;
+  }, 5000);
