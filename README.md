4b4ef55f9b135b26d5960ee00c9930dcc972ec79
Sam N <samathasaurus@gmail.com>
2024-11-19 19:54:08 -0500

    added mantis facts

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 
 Website created via Github Pages with commits pushed from Github Desktop and git bash
 
-Utilizing skills learned via the Responsive Web Design Certification at freeCodeCamp.org and via the Code:You bootcamp.
+Utilizing skills learned via the Responsive Web Design Certification at freeCodeCamp.org and via the Code:You Web Dev bootcamp.
--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function updateDateTime() {
-    const date = new Date();
-    const currentDateTime = date.toLocaleString();
-    document.querySelector('#currentDateTime').textContent = currentDateTime;
+//--- display current date and time ---//
+
+// function updateDateTime() {
+   // const date = new Date();
+   // const currentDateTime = date.toLocaleString();
+   // document.querySelector('#currentDateTime').textContent = currentDateTime;
+// }
+// setInterval(updateDateTime, 1000)
+
+//--- populate facts and prevent repeating facts ---//
+function displayfact() {
+    const facts = ["fact 1","fact 2","fact 3"]
+    const random = Math.floor(Math.random() * facts.length)
+
+    let randomFact = (random, facts[random]);
+
+    document.querySelector('#mantisfacts').textContent = randomFact
 }
-setInterval(updateDateTime, 1000)
+
+setInterval(displayfact, 2000)
